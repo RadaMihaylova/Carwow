4b20da5bf7afcaa37868f8aec8e437af53ca5ec0
adrianlikesatanasov <adrian.i.atanasov.2023@elsys-bg.org>
2024-05-21 20:29:29 +0300

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,18 @@
+mybutton = document.getElementById("goTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener('click', topFunction);
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
