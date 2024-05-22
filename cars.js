6e6362e78f9c0816547ff80242a21013daff8696
RadaMihaylova <mihaylovarada@gmail.com>
2024-05-22 19:51:58 +0300

    Add files via upload

--- cars.js
+++ cars.js
@@ -1,13 +1,21 @@
-mybutton = document.getElementById("goTopBtn");
+var mybutton = document.getElementById("goTopBtn");
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  console.log("Here");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
+
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+  
+  document.getElementById("progressBar").style.width = scrollPercentage + "%";
 }
 
 mybutton.addEventListener('click', topFunction);
